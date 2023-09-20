acf029434d62e35530b3e3f48d4cdcb3050a274b
sebastian <sebastianbarragan02@gmail.com>
2023-09-20 15:13:37 -0500

    Agregan el modelo de roles y coordinations

---
+++ backend/app/Http/Controllers/CoordinationController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Coordination;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CoordinationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Coordination  $coordination
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Coordination $coordination)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Coordination  $coordination
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Coordination $coordination)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Coordination  $coordination
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Coordination $coordination)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Coordination  $coordination
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Coordination $coordination)
+    {
+        //
+    }
+}
